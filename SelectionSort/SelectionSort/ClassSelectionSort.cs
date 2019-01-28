fb162b158b0ddfc3a3ccc82e95e83324bae9a5bb
KravetsWork <workkravets@gmail.com>
2019-01-28 22:07:51 +0200

    Added selection sort algorithm

---
+++ SelectionSort/SelectionSort/ClassSelectionSort.cs
@@ -0,0 +1,29 @@
+﻿namespace SelectionSort
+{
+    /// <summary>
+    /// Class that uses a selection sort algorithm
+    /// </summary>
+    class ClassSelectionSort
+    {
+        public int[] SortSelection(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int minimalElement = array[i];
+                int minimumElementIndex = i;
+                for (int y = i + 1; y < array.Length; y++)
+                {
+                    if (minimalElement > array[y]) // swap elements
+                    {
+                        minimalElement = array[y];
+                        minimumElementIndex = y;
+                        int temp = array[i];
+                        array[i] = array[minimumElementIndex];
+                        array[minimumElementIndex] = temp;
+                    }
+                }
+            }
+            return array;
+        }
+    }
+}
---
+++ SelectionSort/SelectionSort/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test();
+            Console.ReadKey();
+        }
+
+        static void Test()
+        {
+            ClassSelectionSort sort = new ClassSelectionSort();
+            int[] unsortedAraay = { -1, 2, 3, -10, 40, 80, 9, 0 };
+            int[] sortedArray = sort.SortSelection(unsortedAraay);
+            int index = 0;
+            Console.WriteLine("Sorted array: ");
+            foreach (int item in sortedArray)
+                Console.WriteLine($"{index++}| {item}");
+        }
+    }
+}
