7eb419c889f9fca76c6dbe6e601fc795b03af00c
KravetsWork <workkravets@gmail.com>
2019-01-28 21:20:53 +0200

    Changes in binary search

--- BinarySearch/BinarySearch/BinarySearch.cs
+++ BinarySearch/BinarySearch/BinarySearch.cs
@@ -7,22 +7,7 @@ namespace BinarySearch
     /// </summary>
     public class ClassBinarySearch
     {
-        public void Run()
-        {
-            int[] search = { 1, 3, 5, 7, 10, 12, 14, 20, 21, 22, 23, 24, 56, 100, 166, 177 }; // sorted array
-
-            Console.Write("Enter the number to search in the array: ");
-            int item = Convert.ToInt32(Console.ReadLine());
-            int result = AlgorithmBinarySearch(search, item, out int iter);
-            if (result == -1)
-                Console.WriteLine("Item not found!");
-            else
-                Console.WriteLine($"The element is in place: {result} in the array! Number of iterations {iter}.");
-
-            Console.ReadKey();
-        }
-
-        private int AlgorithmBinarySearch(int[] arr, int item, out int iter)
+        public int AlgorithmBinarySearch(int[] arr, int item, out int iter)
         {
             iter = 1;
             int low = 0, high = arr.Length - 1, mid, guess;
--- BinarySearch/BinarySearch/Program.cs
+++ BinarySearch/BinarySearch/Program.cs
@@ -1,11 +1,27 @@
-﻿namespace BinarySearch
+﻿using System;
+
+namespace BinarySearch
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Run();
+            Console.ReadKey();
+        }
+
+        static void Run()
+        {
             ClassBinarySearch binarySearch = new ClassBinarySearch();
-            binarySearch.Run();
+            int[] search = { 1, 3, 5, 7, 10, 12, 14, 20, 21, 22, 23, 24, 56, 100, 166, 177 }; // sorted array
+
+            Console.Write("Enter the number to search in the array: ");
+            int item = Convert.ToInt32(Console.ReadLine());
+            int result = binarySearch.AlgorithmBinarySearch(search, item, out int iter);
+            if (result == -1)
+                Console.WriteLine("Item not found!");
+            else
+                Console.WriteLine($"The element is in place: {result} in the array! Number of iterations {iter}.");
         }
     }
 }
