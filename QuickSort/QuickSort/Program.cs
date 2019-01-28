680d332509278a6af9b37a809687c3ce3aa27449
KravetsWork <workkravets@gmail.com>
2019-01-28 21:47:16 +0200

    Added quick sort algorithm

---
+++ QuickSort/QuickSort/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace QuickSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test();
+            Console.ReadKey();
+        }
+
+        static void Test()
+        {
+            QuickSort quickSort = new QuickSort();
+            int[] numbers = new int[] { 1, 2, 19, -4, -1, 0, 81, -102 };
+            Console.WriteLine("Unsorted array: ");
+            foreach (var number in numbers)
+                Console.Write($"{number}\t");
+
+            var sortNumbers = quickSort.algorithmQuickSort(numbers, 0, numbers.Length - 1);
+            Console.WriteLine("\nSorted array: ");
+            foreach (var sortNum in sortNumbers)
+                Console.Write($"{sortNum}\t");
+        }
+    }
+}
---
+++ QuickSort/QuickSort/QuickSort.cs
@@ -0,0 +1,42 @@
+﻿namespace QuickSort
+{
+    /// <summary>
+    /// Class that uses a quick sort algorithm
+    /// </summary>
+    public class QuickSort
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="left">Bottom element</param>
+        /// <param name="right">Top element</param>
+        /// <returns></returns>
+        public int[] algorithmQuickSort(int[] arr, int left, int right)
+        {
+            int temp;
+            int pivot = arr[left + (right - left) / 2]; // middle element
+            int i = left; 
+            int j = right; 
+
+            while (i <= j) // main sorting pass
+            {
+                while (arr[i] < pivot) i++; // left elements < than average
+                while (arr[j] > pivot) j--; // right elements > than average
+                if (i <= j) // swap items (for example {1, 2, 3, 10, 6(middle element), 7, 8, 3}) swap 10 and 3
+                {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                    i++;
+                    j--;
+                }
+            }
+            // recursively
+            if (i < right) // sorting the right side
+                algorithmQuickSort(arr, i, right);
+            if (left < j) // sorting the left side
+                algorithmQuickSort(arr, left, j);
+            return arr;
+        }
+    }
+}
